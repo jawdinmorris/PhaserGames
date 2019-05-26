c67797855ae83691a0d7fd0468dc698030c5e505
Jordan Morris <jordanandrewmorris1@gmail.com>
2019-05-26 21:01:29 +1000

    Changed controls to mouse, added some bounce cleaned up comments

--- breakout/index.js
+++ breakout/index.js
@@ -54,12 +54,14 @@ function hitPaddle() {
 }
 
 function hitBrick(ball, brick) {
-    console.log("Break");
+    //DELETE BRICKS
     brick.disableBody(true, true);
+    //SCORE
     playerScore += (10 * scoreMultiplier);
     console.log(10 * scoreMultiplier);
     scoreMultiplier += 1;
     playerScoreText.setText("score: " + playerScore + "   x" + scoreMultiplier);
+    //CHECK FOR WIN
     if (bricks.countActive() == 0) {
         isGameOver();
     }
@@ -67,14 +69,15 @@ function hitBrick(ball, brick) {
 
 function isGameOver() {
     if (gameOver == false) {
+        //UPDATE LOST LIFE AND RESET MULTIPLIER
         lives -= 1;
         scoreMultiplier = 1;
         ballVelocity = 0;
         ball.setVelocityY(ballVelocity);
         ball.setVelocityX(ballVelocity);
         playerLifeText.setText("lives: " + lives);
-
         playerScoreText.setText("score: " + playerScore + "   x" + scoreMultiplier);
+        //SEE IF WON OR LOST
         if (bricks.countActive() == 0) {
             playerScoreText.setText(playerScoreText.text + " You Win!");
         } else if (lives == 0) {
@@ -87,7 +90,7 @@ function isGameOver() {
 }
 
 function create() {
-    //player setup
+    //PLAYER SETUP
     this.add.image(0, 0, 'background').setOrigin(0, 0);
     player = this.physics.add.sprite(256, 492, 'paddle').setScale(2);
     player.setInteractive();
@@ -95,23 +98,21 @@ function create() {
     player.body.immovable = true;
     player.setCollideWorldBounds(true);
 
-    //score
+    //TEXT
     playerScoreText = this.add.text(32, 16, 'score: 0   x1', { fontSize: '16px', fill: '#FFF' });
     playerLifeText = this.add.text(320, 16, 'lives: 3', { fontSize: '16px', fill: '#FFF' });
 
-    //player movement
-    cursors = this.input.keyboard.createCursorKeys();
-
-    //ball
-    ball = this.physics.add.sprite(256, 487, 'ball').setOrigin(0, 3).setScale(2);
+    //BALL SETUP
+    ball = this.physics.add.sprite(256, 487, 'ball').setOrigin(0, 3).setScale(2).setCircle(3);
     ball.setCollideWorldBounds(true);
     ball.enableBody = true;
     this.physics.add.collider(ball, player, hitPaddle, null, this);
     ball.setBounce(1);
-    ball.body.maxVelocity.x = 500;
-    ball.body.maxVelocity.y = 500;
+    ball.body.maxVelocity.x = 300;
+    ball.body.maxVelocity.y = 300;
+    ball.setAcceleration(20, 20);
 
-    //Platforms (Level 1)
+    //Platforms (Level 1) - SEPERATE THIS??
     bricks = this.physics.add.group();
     bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50, stepX: 70 }, setScale: { x: 2, y: 2, stepX: 0, stepY: 0 } });
     bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50 + 28, stepX: 70 }, setScale: { x: 2, y: 2, stepX: 0, stepY: 0 } });
@@ -121,38 +122,24 @@ function create() {
         child.enableBody = true;
         child.body.immovable = true;
     });
-
     this.physics.add.collider(ball, bricks, hitBrick, null, this);
+    
+    //START GAME
     roundStart();
 }
+
 function update() {
-    //Player movement
+    //PLAYER MOVEMENT
     if (gameOver == false) {
-        // if (cursors.right.isDown) {
-        //     player.body.setVelocityX(playerVelocity);
-        // } else if (cursors.left.isDown) {
-        //     player.body.setVelocityX(-playerVelocity);
-        // } else {
-        //     player.body.setVelocityX(0);
-        // }
-        // if (this.input.mousePointer.isDown) {
-            if ((this.input.mousePointer.x > player.x - 7) && (this.input.mousePointer.x < player.x + 7)) {
-            //  400 is the speed it will move towards the mous
+        if ((this.input.mousePointer.x > player.x - 7) && (this.input.mousePointer.x < player.x + 7)) {
             player.setVelocityX(0);
             player.setVelocityY(0);
-            //  if it's overlapping the mouse, don't move any more
-            
         } else {
-            this.physics.moveTo(player, this.input.mousePointer.x, 492, 256, null)
+            this.physics.moveTo(player, this.input.mousePointer.x, 492, 512, null)
         }
-
-
-    // }
-    //Death detect
-    if (ball.y > 530) {
-        isGameOver();
+        //DEATH DETECT
+        if (ball.y > 545) {
+            isGameOver();
+        }
     }
-
-}
-
 }
