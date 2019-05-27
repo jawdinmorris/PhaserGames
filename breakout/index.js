e6ca359e7145d7733bbdaaf834a640319aa5eb59
Jordan Morris <jordanandrewmorris1@gmail.com>
2019-05-27 10:50:11 +1000

    Responsive design, but v small now

--- breakout/index.js
+++ breakout/index.js
@@ -2,15 +2,16 @@ playerScore = 0;
 ballVelocity = 200;
 playerVelocity = 600;
 scoreMultiplier = 1;
-
 lives = 3;
-
 gameOver = false;
+
+
+scaleRatio = window.devicePixelRatio / 3;
 
 var config = {
     type: Phaser.AUTO,
-    width: 512,
-    height: 512,
+    width: window.innerWidth * window.devicePixelRatio,
+    height: window.innerHeight * window.devicePixelRatio,
     physics: {
         default: 'arcade',
         arcade: {
@@ -40,8 +41,8 @@ function preload() {
 
 }
 function roundStart() {
-    ball.x = 256;
-    ball.y = 487;
+    ball.x = game.config.width/2;
+    ball.y = game.config.height*.6;
     ballVelocity = 256
     ball.setVelocityY(-ballVelocity);
     ballVelocity = Phaser.Math.Between(-256, 256);
@@ -92,18 +93,18 @@ function isGameOver() {
 function create() {
     //PLAYER SETUP
     this.add.image(0, 0, 'background').setOrigin(0, 0);
-    player = this.physics.add.sprite(256, 492, 'paddle').setScale(2);
+    player = this.physics.add.sprite(game.config.width/2, game.config.height*.85, 'paddle').setScale(scaleRatio, scaleRatio);
     player.setInteractive();
     player.setOrigin(0.5, 0);
     player.body.immovable = true;
     player.setCollideWorldBounds(true);
 
     //TEXT
-    playerScoreText = this.add.text(32, 16, 'score: 0   x1', { fontSize: '16px', fill: '#FFF' });
-    playerLifeText = this.add.text(320, 16, 'lives: 3', { fontSize: '16px', fill: '#FFF' });
+    playerScoreText = this.add.text(game.config.width*.1, 16, 'score: 0   x1', { fontSize: '16px', fill: '#FFF' });
+    playerLifeText = this.add.text(game.config.width*.6, 16, 'lives: 3', { fontSize: '16px', fill: '#FFF' });
 
     //BALL SETUP
-    ball = this.physics.add.sprite(256, 487, 'ball').setOrigin(0, 3).setScale(2).setCircle(3);
+    ball = this.physics.add.sprite(game.config.width/2, game.config.height*.6, 'ball').setOrigin(0, 3).setScale(scaleRatio, scaleRatio).setCircle(3);
     ball.setCollideWorldBounds(true);
     ball.enableBody = true;
     this.physics.add.collider(ball, player, hitPaddle, null, this);
@@ -114,9 +115,9 @@ function create() {
 
     //Platforms (Level 1) - SEPERATE THIS??
     bricks = this.physics.add.group();
-    bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50, stepX: 70 }, setScale: { x: 2, y: 2, stepX: 0, stepY: 0 } });
-    bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50 + 28, stepX: 70 }, setScale: { x: 2, y: 2, stepX: 0, stepY: 0 } });
-    bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50 + 56, stepX: 70 }, setScale: { x: 2, y: 2, stepX: 0, stepY: 0 } });
+    bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50, stepX: 70 }, setScale: { x: scaleRatio, y: scaleRatio, stepX: 0, stepY: 0 } });
+    bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50 + 28, stepX: 70 }, setScale: { x: scaleRatio, y: scaleRatio, stepX: 0, stepY: 0 } });
+    bricks.createMultiple({ key: 'brick', repeat: 6, setXY: { x: 45, y: 50 + 56, stepX: 70 }, setScale: { x: scaleRatio, y: scaleRatio, stepX: 0, stepY: 0 } });
     console.log(bricks.countActive());
     bricks.children.iterate(function (child) {
         child.enableBody = true;
@@ -135,10 +136,10 @@ function update() {
             player.setVelocityX(0);
             player.setVelocityY(0);
         } else {
-            this.physics.moveTo(player, this.input.mousePointer.x, 492, 512, null)
+            this.physics.moveTo(player, this.input.mousePointer.x, game.config.height*.85, 512, null)
         }
         //DEATH DETECT
-        if (ball.y > 545) {
+        if (ball.y > game.config.height *1.1) {
             isGameOver();
         }
     }
